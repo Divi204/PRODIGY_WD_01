10de28d9ed6bf2eb42b54c0b25f59170da7e9f7d
Divi204 <142157935+Divi204@users.noreply.github.com>
2024-11-02 19:05:58 +0530

    Add files via upload

---
+++ c.js
@@ -0,0 +1,4 @@
+// You can add interactivity here if needed
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('Thank you for your interest! We will get back to you shortly.');
+});
